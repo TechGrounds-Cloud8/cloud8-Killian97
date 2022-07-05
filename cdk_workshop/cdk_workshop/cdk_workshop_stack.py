1b90f7f8937d3d09542db927f2ff792230cea792
Killian97 <104758332+Killian97@users.noreply.github.com>
2022-07-05 11:11:12 +0200

    cdk workshop

--- cdk_workshop/cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop/cdk_workshop_stack.py
@@ -1,6 +1,7 @@
 from constructs import Construct
 from aws_cdk import (
     Stack,
+    aws_lambda as _lambda,
 )
 
 
@@ -8,3 +9,12 @@ class CdkWorkshopStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        # defines an aws lambda resourse
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.from_asset('lambda'),
+            handler='hello.handler',
+        )
+
