49e141253eb783a29f561fbd80430b2716945400
Killian97 <104758332+Killian97@users.noreply.github.com>
2022-07-11 10:21:33 +0200

    projectv1.0 update

--- project/projectv1_0/projectv1_0/projectv1_0_stack.py
+++ project/projectv1_0/projectv1_0/projectv1_0_stack.py
@@ -1,8 +1,13 @@
+from multiprocessing.sharedctypes import Value
 from aws_cdk import (
+    CfnOutput,
     Stack,
     aws_ec2 as ec2,
 )
 from constructs import Construct
+
+with open("./post_launch_scripts/web_userdata.sh") as f:
+    web_userdata = f.read()
 
 class Projectv10Stack(Stack):
 
@@ -23,10 +28,11 @@ class Projectv10Stack(Stack):
                 ec2.SubnetConfiguration(
                     name="Public", 
                     cidr_mask=26, 
-                    subnet_type=ec2.SubnetType.PUBLIC),
+                    subnet_type=ec2.SubnetType.PUBLIC)
             ]
         )
 
+        # Create and configure webserver ec2 instance
         web_instance = ec2.Instance(
             self, "Web-Instance",
             instance_type=ec2.InstanceType("t2.micro"),
@@ -35,9 +41,34 @@ class Projectv10Stack(Stack):
                 subnet_type=ec2.SubnetType.PUBLIC
             ),
             machine_image=ec2.AmazonLinuxImage(
-                generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-            )
-            user_data=
+                generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2
+            ),
+            user_data=ec2.UserData.custom(web_userdata)
         )
-        # kan dingen later na de instance creation ook nog toevoegen
-        # web_instance.add_user_data
+
+        web_instance.connections.allow_from_any_ipv4(
+            ec2.Port.tcp(80)
+        )
+
+        CfnOutput(self, "Output",
+            value=web_instance.instance_public_ip
+        )
+
+
+
+        #######################################
+        ###Everything for the adminserver VPC##
+        #######################################
+
+        vpc_manageserver = ec2.Vpc(
+            self, "manage-prd-vpc",
+            cidr="10.20.20.0/24",
+            max_azs=2,
+            nat_gateways=0,
+            subnet_configuration=[
+                ec2.SubnetConfiguration(
+                    name="Public", 
+                    cidr_mask=26, 
+                    subnet_type=ec2.SubnetType.PUBLIC)
+            ]
+        )        
